ed251dc1d9aa01829f85e8fa4546a3da835d2476
nicacio16 <32529988+nicacio16@users.noreply.github.com>
2021-09-12 12:58:37 -0300

    versão 1.2 - Erro corrigido
    O erro estava em ProjetoES/testes/urls.py
    
    "path('testes/upload/', UploadCreate.as_view(), name="upload-imagem")"
    
    estava chamando UploadView ao invés de UploadCreate.

--- testes/urls.py
+++ testes/urls.py
@@ -1,9 +1,7 @@
-from ProjetoES.paginas.views import UploadView
 from django.urls import path
-
 from .views import UploadCreate
 
 urlpatterns = [
     #Exemplo: path('', IndexView.as_view(), name='inicio'),
-    path('testes/upload/', UploadView.as_view(), name="upload-imagem")
+    path('testes/upload/', UploadCreate.as_view(), name="upload-imagem")
 ]
